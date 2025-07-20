a20a3271faee641327ca38e1fd96b129ab18da1a
james-otten <jamesotten1@gmail.com>
2025-07-20 12:37:46 -0400

    updates

--- roles/vpp/files/setup_vppcfg.sh
+++ roles/vpp/files/setup_vppcfg.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 cd /root/vppcfg/
 source /root/vppcfg_venv/bin/activate
 make install-deps
 make test
 make build
 deactivate
-make install
+export PIP_BREAK_SYSTEM_PACKAGES=1
+#make install
+pip install ./dist/vppcfg-0.0.8-py3-none-any.whl
 vppcfg -h
